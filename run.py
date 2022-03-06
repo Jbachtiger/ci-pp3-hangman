aaeb4bc935fdb77a18efdb09b335a6217929e54c
Jbachtiger <jbachtiger@gmail.com>
2022-03-06 14:41:13 +0000

    Add welcome message

--- run.py
+++ run.py
@@ -5,3 +5,5 @@ Imports section
 from words import easy_words
 from words import hard_words
 
+# Welcome message
+print("Welcome to Hangman. Test your word knowledge and see if you can avoid the noose! Guess the word and win.\n")

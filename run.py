a052f58929d5ab03ea4b0ba8ccf7b2f562b5a63a
Jbachtiger <jbachtiger@gmail.com>
2022-03-06 14:50:25 +0000

    Add player_name and validate_player_name functions

--- run.py
+++ run.py
@@ -7,3 +7,32 @@ from words import hard_words
 
 # Welcome message
 print("Welcome to Hangman. Test your word knowledge and see if you can avoid the noose! Guess the word and win.\n")
+
+def player_name(): 
+    """
+    Allows the user to input their name
+    """
+    while True:        
+        player_data = input("Enter your name: ")
+
+        if validate_player_name(player_data):
+            print(f"Welcome to Hangman {player_data} \n")
+            break
+
+def validate_player_name(name):
+    """
+    Validate player name input so it cannot be left blank
+    """
+    try:
+        if name == "":
+            raise ValueError("invalid input. Please type your name e.g. John \n")
+        elif len(name) == 0:
+            raise ValueError("invalid input. Please type your name e.g. John \n")
+
+    except ValueError as error:
+        print(f"Try again, {error}")
+        return False
+
+    return True
+
+player_name()

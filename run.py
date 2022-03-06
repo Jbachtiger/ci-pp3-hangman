15cd60f8bd3128a9589f786f6bd7e7dfa2e64d76
Jbachtiger <jbachtiger@gmail.com>
2022-03-06 11:58:47 +0000

    Add words list file and corresponding import statements

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Imports section
+"""
+# Import statements I created for the game
+from words import easy_words
+from words import hard_words
+
---
+++ words.py
@@ -0,0 +1,49 @@
+"""
+Random list of words
+"""
+
+easy_words = [
+    "JOKE",
+    "SPACE",
+    "PLANT",
+    "JUMP",
+    "BACK",
+    "CRACK",
+    "POSH",
+    "BUZZ",
+    "ACTOR",
+    "BEAM",
+    "BEAN",
+    "BEAR",
+    "CLAW",
+    "CLOG",
+    "DATA",
+    "CODE",
+    "FELL",
+    "GEEK",
+    "HAWK",
+    "MOOD"
+]
+
+hard_words = [
+    "ABRUPTLY",
+    "AFFIX",
+    "SUBWAY",
+    "MATRIX",
+    "GLOWWORM",
+    "JIGSAW",
+    "STRENGTH",
+    "ZOMBIE",
+    "HANGMAN",
+    "WHISKEY",
+    "GARAGE",
+    "STAIRS",
+    "KITCHEN",
+    "TRANSCRIPT",
+    "ROCKING",
+    "PLANETS",
+    "SPACESHIP",
+    "JOGGING",
+    "PYTHON",
+    "WIZARD"
+]

3e9bad4bc6c1420e4fb96611ca5d49ad4d653083
Jbachtiger <jbachtiger@gmail.com>
2022-03-16 16:20:02 +0000

    Remove commented out code

--- run.py
+++ run.py
@@ -9,20 +9,10 @@ from words import easy_words, hard_words
 from hangman_art import hangman_pics, hangman_logo
 
 
-# def clear_console():
-#     """
-#     Clears the terminal
-#     Credit - clear console code used from
-#     https://www.delftstack.com/howto/python/python-clear-console/
-#     """
-#     command = 'clear'
-#     if os.name in ('nt', 'dos'):
-#         command = 'cls'
-#     os.system(command)
-
 def clear_console():
     """
-    Clears terminal
+    Clears the terminal
+    Credit - Code Institute Tutor Support
     """
     os.system('printf "\ec"')
 
